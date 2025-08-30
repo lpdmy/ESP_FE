e7096384c11a6c75660bea3cca496e4dbe3f5705
lpdmy <dieumy430@gmail.com>
2025-08-30 21:28:09 +0700

    add BE URL

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,7 +1,7 @@
 // API Configuration
 export const API_CONFIG = {
    // Base URLs
-   BASE_URL: import.meta.env.PROD ? '/api' : 'https://localhost:7084/api',
+   BASE_URL: import.meta.env.PROD ? import.meta.env.API_URL : 'https://localhost:7084/api',
 
    // Auth endpoints
    AUTH: {
