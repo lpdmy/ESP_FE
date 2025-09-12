33d2206754befb7fcffb62ed028fe9fbe98203a6
HongPhuc03 <topluaq@gmail.com>
2025-09-12 23:03:28 +0700

    remove not relative code

--- src/components/LandingPage/Header.jsx
+++ src/components/LandingPage/Header.jsx
@@ -2,9 +2,7 @@ import { Bell, Search, User, Menu, Calendar, Trophy, Star } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Badge } from "@/components/ui/badge"
 import { Input } from "@/components/ui/input"
-import { useNavigate } from "react-router-dom"
 export default function Header() {
-  const navigate = useNavigate()
   return (
     <header className="bg-white shadow-sm border-b border-gray-100 px-6 py-4 sticky top-0 z-50">
       <div className="flex items-center justify-between">
@@ -32,9 +30,7 @@ export default function Header() {
 
         {/* Navigation Icons */}
         <div className="flex items-center space-x-4">
-          <Button variant="ghost" className="flex items-center space-x-2 hover:bg-orange-50"
-          onClick={() => navigate("/activity/list-event")}
-          >
+          <Button variant="ghost" className="flex items-center space-x-2 hover:bg-orange-50">
             <Calendar className="h-4 w-4" />
             <span>Sự kiện</span>
           </Button>
