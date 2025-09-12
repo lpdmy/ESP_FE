e0715812988ff9a36a929b8460e0cd93188a52b9
HongPhuc03 <topluaq@gmail.com>
2025-09-13 02:40:29 +0700

    fix deploy

--- src/pages/Event/page.jsx
+++ src/pages/Event/page.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import Header from "@/components/LandingPage/Header"
-import Sidebar from "@/components/LandingPage/sidebar"
+import Sidebar from "@/components/LandingPage/Sidebar"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
