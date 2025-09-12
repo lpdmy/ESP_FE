8b1bd6a5a3d25fd075075b515042eafe90901842
HongPhuc03 <topluaq@gmail.com>
2025-09-12 23:41:13 +0700

    fix deploy

--- src/pages/Auth/ForgotPassword/page.jsx
+++ src/pages/Auth/ForgotPassword/page.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/Input"
+import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/Label"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/Card"
 import { ArrowLeft, Mail, GraduationCap, CheckCircle } from "lucide-react"
