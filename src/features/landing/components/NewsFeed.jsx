67aaa8de400dd28014f8be7dfac1529af3373b71
CallMeHAF <79244066+haf169@users.noreply.github.com>
2025-09-18 11:40:54 +0700

    bug/ESP78 Done

--- src/features/landing/components/NewsFeed.jsx
+++ src/features/landing/components/NewsFeed.jsx
@@ -2,7 +2,7 @@ import { Plus, Image, Video, Smile } from "lucide-react"
 import { Button } from "@/common/components/ui/button"
 import { Card } from "@/common/components/ui/card"
 import { Input } from "@/common/components/ui/input"
-import PostCard from "@LandingPage/PostCard"
+import PostCard from "./PostCard"
 
 export default function NewsFeed() {
   return (
@@ -49,7 +49,7 @@ export default function NewsFeed() {
         class="12A2"
         time="2 giờ trước"
         content="Vừa hoàn thành tác phẩm vẽ tranh cho cuộc thi 'Màu sắc tuổi trẻ' 🎨 Mong mọi người ủng hộ em nhé!"
-        image="/placeholder.svg?height=300&width=500"
+        image="../../../../public/Picturemockdata/DSC04766.jpg"
         likes={24}
         comments={8}
         shares={3}
@@ -73,7 +73,7 @@ export default function NewsFeed() {
         class="12A1"
         time="1 ngày trước"
         content="Chúc mừng đội văn nghệ lớp mình đã giành giải nhất cuộc thi 'Tài năng trẻ FPT' 🏆 Cảm ơn sự ủng hộ của mọi người!"
-        image="/placeholder.svg?height=250&width=400"
+        image="../../../../public/Picturemockdata/DSC03778.jpg"
         likes={45}
         comments={20}
         shares={8}
@@ -85,7 +85,7 @@ export default function NewsFeed() {
         class="10A4"
         time="2 ngày trước"
         content="Tác phẩm nhiếp ảnh 'Góc nhìn tuổi trẻ' của em tham gia cuộc thi nhiếp ảnh năm nay. Mọi người cho em ý kiến nhé! 📸"
-        image="/placeholder.svg?height=350&width=500"
+        image="../../../../public/Picturemockdata/IMG_1492.jpg"
         likes={32}
         comments={15}
         shares={6}
