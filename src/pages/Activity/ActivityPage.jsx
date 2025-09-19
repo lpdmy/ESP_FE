f8e9908160622fda48259f85ccc31f878bf693bd
lpdmy <dieumy430@gmail.com>
2025-09-19 20:11:31 +0700

    ESP77 - MyLPD - fix: loading in Student Profile, update button save, delete console.log

---
+++ src/pages/Activity/ActivityPage.jsx
@@ -0,0 +1,10 @@
+import ListActivity from "@/features/activity/components/listActivity";
+import SidebarLayout from "@/common/components/layout/SidebarLayout";
+
+export default function ActivityPage() {
+    return (
+        <SidebarLayout>
+            <ListActivity />
+        </SidebarLayout>
+    );
+}
