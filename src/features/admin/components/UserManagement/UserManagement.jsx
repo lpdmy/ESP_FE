70e13273576284ed5f7a9bdc1fdd1070d71c62b6
CaptainMarvelouss <phanduonghuy2003@gmail.com>
2025-09-28 22:55:12 +0700

    Fix birthday

--- src/features/admin/components/UserManagement/UserManagement.jsx
+++ src/features/admin/components/UserManagement/UserManagement.jsx
@@ -941,6 +941,7 @@ export default function UserManagement() {
                       type="date"
                       value={newUser.birthDate}
                       onChange={(e) => setNewUser({ ...newUser, birthDate: e.target.value })}
+                      max={new Date(new Date().getFullYear() - 14, 11, 31).toISOString().split('T')[0]}
                       className="!h-10 !w-full !rounded-md !border !border-gray-300 !bg-white !px-3 !py-2 !text-sm"
                     />
                     {formErrors.birthDate && <span className="!text-red-500 !text-xs !mt-1">{formErrors.birthDate}</span>}
@@ -1319,6 +1320,7 @@ export default function UserManagement() {
                       type="date"
                       value={newUser.birthDate}
                       onChange={(e) => setNewUser({ ...newUser, birthDate: e.target.value })}
+                      max={new Date(new Date().getFullYear() - 14, 11, 31).toISOString().split('T')[0]}
                       className="!h-10 !w-full !rounded-md !border !border-gray-300 !bg-white !px-3 !py-2 !text-sm"
                     />
                     {formErrors.birthDate && <span className="!text-red-500 !text-xs !mt-1">{formErrors.birthDate}</span>}
