f7ac53a350ebf2d278f10a2365fd1c142a8282b4
HongPhuc03 <topluaq@gmail.com>
2025-10-26 00:34:26 +0700

    Change Name file

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -1,6 +1,6 @@
 import { Route } from "react-router-dom";
 import UserManagementPage from "@/pages/Admin/UserManagementPage";
-import ClassManagementPage from "@/pages/Admin/ClassManagementPage";
+import ClubManagementPage from "@/pages/Admin/ClassManagementPage";
 import DashboardPage from "@/pages/Admin/DashboardPage";
 import ImportStudentsPage from "@/pages/Admin/ImportStudentsPage";
 import { ROLE } from "@/common/constants/roles";
@@ -14,6 +14,6 @@ export const adminRoutes = [
         <Route key="user-management" path={ROUTES.ADMIN.USER_MANAGEMENT} element={<UserManagementPage />} />,
         <Route key="import-students" path={ROUTES.ADMIN.IMPORT_USERS} element={<ImportStudentsPage />} />
         <Route key="star-point" path={ROUTES.ADMIN.REWARDS} element={<RewardManagementPage />} />
-        <Route key="club-manage" path={ROUTES.ADMIN.CLUB} element={<ClassManagementPage />} />
+        <Route key="club-manage" path={ROUTES.ADMIN.CLUB} element={<ClubManagementPage />} />
     </Route>
 ];
